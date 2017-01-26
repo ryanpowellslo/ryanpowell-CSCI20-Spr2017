e11261cfff93586025b107f2efd19be6d7ca38b9
Ryan Powell <rpowell007@student.butte.edu>
2017-01-26 22:44:25 +0000

    Lab 1.1 Submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,10 @@
+Computer randomly generates two positive integers with a difference of at least 11.
+Computer randomly selects an integer 'x' between the two integers.
+Computer gives user a starting score of 10 points.
+Computer asks user to guess a number between the two randomly generated integers.
+If user guess is less than x then the computer will prompt user to guess higher and subtract one point from total score.
+If user guess is higher than x then the computer will prompt user to guess lower and subtract one point from total score.
+If the user guesses correctly then the computer will let the user know they won and output their score.
+If total score reaches 0 then the computer will let the user know they lost and ask if they would like to play again.
+If user accepts request the computer returns to step one.
+If user declines request the computer ends the program.
