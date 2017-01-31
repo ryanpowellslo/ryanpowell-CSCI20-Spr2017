89aef669b3b8d6818441615d03fe233e8a200acd
Ryan Powell <rpowell007@student.butte.edu>
2017-01-31 22:41:52 +0000

    Lab 1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,31 @@
+//Ryan Powell Lab12 1/31/17
+
+
+#include <iostream> 
+using namespace std;
+
+int main(){
+    cout << "           8 8 8 8 " << endl;
+    cout << "         8         8" << endl;
+    cout << "        8           8" << endl;
+    cout << "        8           8" << endl;
+    cout << "        8           8" << endl;
+    cout << "        8           8" << endl;
+    cout << "         8         8 " << endl;
+    cout << "          8       8" << endl;
+    cout << "           8     8" << endl; 
+    cout << "            8   8" << endl;
+    cout << "              8" << endl;
+    cout << "            8   8" << endl;
+    cout << "           8     8" << endl;
+    cout << "          8       8" << endl;
+    cout << "         8         8" << endl;
+    cout << "        8           8" << endl;
+    cout << "        8           8" << endl;
+    cout << "        8           8"  << endl;
+    cout << "        8           8" << endl;
+    cout << "         8         8" << endl;
+    cout << "          8       8" << endl;
+    cout << "           8 8 8 8" << endl;
+    
+}  
