068c5a33ebf256cb44fbae5abcd2efaee97841be
Ryan Powell <rpowell007@student.butte.edu>
2017-02-08 02:44:48 +0000

    Lab 1.4 Submission

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -15,19 +15,19 @@ int main() {
     
     cout << "Enter in cash calue of coins to be counted in cents : " << endl;
     cin >> coinsInput;
-    moneyPaid = coinsInput * 0.891 / 100;
+    moneyPaid = static_cast<double>(coinsInput) * 0.891 / 100;
     quarters = coinsInput / 25;
-    coinsInput = coinsInput - (quarters * 25);
+    coinsInput = coinsInput % 25;
     dimes = coinsInput / 10;
-    coinsInput = coinsInput - (dimes * 10);
+    coinsInput = coinsInput % 10;
     nickels = coinsInput / 5;
-    coinsInput = coinsInput - (nickels * 5);
+    coinsInput = coinsInput % 5;
     pennies = coinsInput / 1;
     cout << "You deposited " << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, and " << pennies << " pennies." <<endl;
     cout << "You receive a voucher for $" << moneyPaid << ". Thank you!" << endl;
     
     
-    //I'm going to rewrite this using modulo i just wanted to get my foundation laid out.
+    //I'm having a hard time reducing the sig figs on moneyPaid, every time I try to use the % operator with that variable i get an error
     
     return 0;
 }
