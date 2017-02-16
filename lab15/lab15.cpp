8236f3072913519d3df369f03abc5a223ead66ef
Ryan Powell <rpowell007@student.butte.edu>
2017-02-16 23:00:40 +0000

    Lab1.5 Submission

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 int main(){
-    
+    //Here is where I define and initialize my strings for the mad libs
     string lib1 = "";
     string lib2 = "";
     string lib3 = "";
@@ -15,6 +15,7 @@ int main(){
     string lib5 = "";
     
     cout << "Welcome to Mad Libs! Let's get started!" << endl;
+    //I begin getting the libs using cin to allow the user to input unique words
     cout << "First we'll need a noun: ";
     cin >> lib1;
     cout << "Next let's get an adjective: ";
@@ -25,6 +26,7 @@ int main(){
     cin >> lib4;
     cout << "And last but not least enter in your favorite number: ";
     cin >> lib5;
+    //now that I have all the input I need I insert the appropraite libs into the story to complete it   
     cout << "Here we go!" << endl;
     cout << "My " << lib1 << " is so " << lib2 << ", and all of my friends love it!" << endl;
     cout << "I took it with me to " << lib3 << " last summer where I " << lib4 << " with it everywhere!" << endl;
